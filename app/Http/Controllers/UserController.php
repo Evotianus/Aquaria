4c9b8fda7d1c7ca97040f1ea657a68934d1be1b5
sterben404 <fredy.sunjaya@binus.ac.id>
2024-02-13 13:17:45 +0700

    add,update,view,delete task

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -40,10 +40,36 @@ class UserController extends Controller
         $user = User::where('username', $request->username)->where('password', $request->password)->first()->get();
 
         if ($user->count() != 0) {
-            return $user;
+            return $user[0];
         }
 
         return response()->json(['Message' => 'Login Error'], 400);
+    }
+
+    public function deleteUser(Request $request) {
+        $deletedUser = User::where('email', $request->email )
+        ->delete();
+    }
+
+    public function changeUsername(Request $request) {
+        $updatedName = User::where('email', $request->email)
+        ->update([
+            'username' => $request->username
+        ]);
+    }
+
+    public function changeEmail(Request $request) {
+        $updatedEmail = User::where('email', $request->email)
+        ->update([
+            'email' => $request->newEmail
+        ]);
+    }
+
+    public function changePassword(Request $request) {
+        $updatedPassword = User::where('email', $request->email)
+        ->update([
+            'password' => $request->newPassword
+        ]);
     }
 
     /**
